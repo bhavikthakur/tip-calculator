da8af96810c28c917131ce39f3d168006bb427ad
Bhavik Thakur <thakursbhavik@gmail.com>
2025-04-22 04:08:59 +0530

    update code

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const totalTipPerson = document.querySelector(".result__tip--person");
 const totalTipAmount = document.querySelector(".result__tip--total");
 const resetBtn = document.querySelector(".result__btn--reset");
 const form = document.querySelector(".form");
+const errorMessage = document.querySelector(".form__error");
+const customInput = document.querySelector(".form__btn--tip-custom");
+const personContainer = document.querySelector(".form__people-wrapper");
 
 allTipButtons.forEach((tipButton) => {
   tipButton.addEventListener("click", (evt) => {
     const targetedTip = Number(evt.target.value); // exact tip button
     const billInputValue = Number(billInput.value); // total bill
+    const customInputValue = Number(customInput.value);
 
     // calculate total tip
     function calculateTipFunc(billInput, targetedTip) {
@@ -19,26 +23,38 @@ allTipButtons.forEach((tipButton) => {
 
     const totalTip = calculateTipFunc(billInputValue, targetedTip); // total tip in variable
 
-    // calculate total tip
-    function showTotalTip() {
-      totalTipAmount.innerText = `$${totalTip.toFixed(2)}`;
-    }
-    showTotalTip();
-
     peopleInput.addEventListener("input", () => {
       const peopleInputValue = Number(peopleInput.value); // total person
+
       // calculate per person tip
       function totalTipPerPerson(person) {
         return totalTip / person;
       }
+      // calculate total tip
+      function showTotalTip() {
+        totalTipAmount.innerText = `$${totalTip.toFixed(2)}`;
+        totalTipPerson.innerText = `$${TipPerPerson.toFixed(2)}`;
+      }
       const TipPerPerson = totalTipPerPerson(peopleInputValue);
-      totalTipPerson.innerText = `$${TipPerPerson.toFixed(2)}`;
+
+      if (peopleInputValue === 0) {
+        defaultText();
+        errorMessage.classList.remove("hidden");
+        personContainer.classList.add("outline__error");
+      } else {
+        showTotalTip();
+        errorMessage.classList.add("hidden");
+        personContainer.classList.remove("outline__error");
+      }
     });
   });
 });
+function defaultText() {
+  totalTipPerson.innerText = "$0.00";
+  totalTipAmount.innerText = "$0.00";
+}
 
 resetBtn.addEventListener("click", () => {
   form.reset();
-  totalTipPerson.innerText = "$0.00";
-  totalTipAmount.innerText = "$0.00";
+  defaultText();
 });
